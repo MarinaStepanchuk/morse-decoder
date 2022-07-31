9f65bdaa8d6602aea91e2e057251a7fe934ea70d
MarinaStepanchuk <mari20002@mail.ru>
2022-07-31 23:21:27 +0300

    ready

--- src/index.js
+++ src/index.js
@@ -37,9 +37,58 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
-function decode(expr) {
-    // write your solution here
+let smashString = (str) => {
+    let array = [];
+    for (let i = 0; i < str.length; i = i+10) {
+    array.push(str.slice(i, i+10));
+    };
+    return array;
 }
+
+let decodeVal = (item) => {
+    let plus = '';
+    for (let j = 0; j < (item.length - 1); j = j + 2){
+        if (item[j] === '1' && item[j+1] === '0') {
+            plus += '.';
+        } else if(item[j] === '1' && item[j+1] === '1') {
+            plus += '-';
+        };
+    };
+    return plus;
+};
+
+let findCode = (item) => {
+    for (key in MORSE_TABLE) {
+        if (item === key) {
+            return MORSE_TABLE[key];
+        };
+    };
+};
+
+function decode(expr) {
+    let smashTen = smashString(expr);
+    
+    let arrCode = smashTen.map(value => {
+        if (value === '**********') {
+            return ' '
+        } else {
+            return decodeVal(value)   
+        };
+    });
+
+    let message = arrCode.reduce((strCode, value) => {
+        if(value !== ' ') {
+            strCode += findCode(value) 
+        } else {
+            strCode += value
+        };
+        return strCode;
+
+    },'');
+
+    return message;
+}
+
 
 module.exports = {
     decode
